a5409ab6821093aedd648d2fc0acea5ac3ed8ed7
villagerunknown <VillagerUnknown@proton.me>
2025-03-26 06:05:16 -0400

    Added ProfileUtil

---
+++ src/main/java/me/villagerunknown/platform/util/ProfileUtil.java
@@ -0,0 +1,9 @@
+package me.villagerunknown.platform.util;
+
+import java.util.UUID;
+
+public class ProfileUtil {
+	
+	public static final UUID DEFAULT_UUID = UUID.fromString("5aa7ebdf-c67b-4643-9214-3015d269dedb");
+
+}
