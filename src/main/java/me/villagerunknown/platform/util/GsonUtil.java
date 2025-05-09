d0a9de9636f9116e2c6bd90aea7fc454072603cd
villagerunknown <VillagerUnknown@proton.me>
2025-05-09 15:33:42 -0400

    Importing type adapter from Villager Coin

--- src/main/java/me/villagerunknown/platform/util/GsonUtil.java
+++ src/main/java/me/villagerunknown/platform/util/GsonUtil.java
@@ -5,30 +5,33 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 import me.villagerunknown.platform.Platform;
+import me.villagerunknown.platform.adapter.ItemCountMapTypeAdapter;
 import me.villagerunknown.platform.adapter.OptionalGlobalPosTypeAdapter;
+import me.villagerunknown.platform.adapter.ProfileResultMapTypeAdapter;
 import me.villagerunknown.platform.adapter.WorldPositionsMapTypeAdapter;
+import me.villagerunknown.platform.data.ProfileResultData;
+import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.GlobalPos;
 
 import java.lang.reflect.Type;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class GsonUtil {
 	
-	public static Map<Type, Object> ADAPTERS = new HashMap<>() {{
+	public static final Map<Type, Object> ADAPTERS = new HashMap<>() {{
+		put(new TypeToken<Map<Item, Integer>>(){}.getType(), new ItemCountMapTypeAdapter());
 		put(new TypeToken<Optional<GlobalPos>>(){}.getType(), new OptionalGlobalPosTypeAdapter());
+		put(new TypeToken<Map<UUID, ProfileResultData>>(){}.getType(), new ProfileResultMapTypeAdapter());
 		put(new TypeToken<Map<String, Set<BlockPos>>>(){}.getType(), new WorldPositionsMapTypeAdapter());
 	}};
 	
-	public static GsonBuilder registerAdapters( GsonBuilder gsonBuilder ) {
-		ADAPTERS.forEach(( Type type, Object object ) -> {
-			gsonBuilder.registerTypeAdapter( type, object );
-		});
-		
-		return gsonBuilder;
+	public static void registerAdapters( GsonBuilder gsonBuilder ) {
+		if( null != ADAPTERS && !ADAPTERS.isEmpty() ) {
+			ADAPTERS.forEach(gsonBuilder::registerTypeAdapter);
+		} else {
+			Platform.LOGGER.warn( "GSON Type Adapters are missing. Verify that included type adapters are working properly." );
+		} // if, else
 	}
 	
 	public static Gson gsonForFiles() {
@@ -40,9 +43,7 @@ public class GsonUtil {
 	public static Gson gsonWithAdapters() {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		
-		ADAPTERS.forEach(( Type type, Object object ) -> {
-			gsonBuilder.registerTypeAdapter( type, object );
-		});
+		registerAdapters(gsonBuilder);
 		
 		return gsonBuilder.create();
 	}
@@ -50,10 +51,10 @@ public class GsonUtil {
 	public static Gson gsonWithAdapter( Type type ) {
 		GsonBuilder gsonBuilder = new GsonBuilder();
 		
-		if( ADAPTERS.containsKey( type ) ) {
+		if( null != ADAPTERS && ADAPTERS.containsKey( type ) ) {
 			gsonBuilder.registerTypeAdapter( type, ADAPTERS.get( type ) );
 		} else {
-			Platform.LOGGER.warn( "Missing Type Adapter: " + type );
+			Platform.LOGGER.warn( "Missing GSON Type Adapter: " + type );
 		} // if
 		
 		return gsonBuilder.create();
